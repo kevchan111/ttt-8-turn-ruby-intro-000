7d935b8acbe5edc0287628ba8d6483e76e032e0b
Kevin Chan <kevchan111@gmail.com>
2018-04-12 16:02:43 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(str)
+str.to_i - 1
+end
+
+
+def move(display_board, index, value)
+  display_board[index] = value
+end
+
+
+def position_taken?(board, index)
+   if index >= 0 && index <= 8
+  true
+  else
+    false
+ end
+ end
+
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+elsif board[index] == "X" || board[index] == "O"
+  return true
+end
+end
+
+
+def valid_move?(board , index)
+ if position_taken?(board , index)
+    return false
+  else
+    return true
+ end
+end
